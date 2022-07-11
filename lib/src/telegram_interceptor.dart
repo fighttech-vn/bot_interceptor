4df1cbb8e69e6375a35f5177f81aa08a90bd50e5
trunghieu <trunghieu@trunghieu.local>
2022-07-11 17:57:12 +0700

    feat(Telegram): support send telegram on response

--- lib/src/telegram_interceptor.dart
+++ lib/src/telegram_interceptor.dart
@@ -2,25 +2,55 @@ import 'package:dio/dio.dart';
 import 'package:uuid/uuid.dart';
 
 import 'send_message/impl/telegram_send_message_provider.dart';
+
 class TelegramInterceptor extends Interceptor {
   final String token;
   final int chatId;
   final String? projectId;
+  final bool willSendSuccess;
 
   TelegramInterceptor({
     required this.token,
     required this.chatId,
     this.projectId,
-  });
-
-  @override
-  void onError(DioError err, ErrorInterceptorHandler handler) {
-    final bot = TelegramSendMessageProvider(
+    this.willSendSuccess = false,
+  }) {
+    _messageProvider = TelegramSendMessageProvider(
       token: token,
       chatId: chatId,
     );
+  }
+
+  late TelegramSendMessageProvider _messageProvider;
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
     final projectName = projectId != null ? '#${projectId!}' : '';
-    bot.send(
+
+    if (willSendSuccess) {
+      _messageProvider.send(
+        message: '''
+$projectName ${const Uuid().v4()}
+<code>{
+  'from': 'onResponse',
+  'Time': ${DateTime.now().toString()},
+  'baseUrl': '${response.requestOptions.baseUrl}',
+  'path': ${response.requestOptions.path},
+  'path': ${response.requestOptions.path},
+  'method': ${response.requestOptions.method},
+   'responseData': ${response.data},
+}</code>
+    ''',
+      );
+    }
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    final projectName = projectId != null ? '#${projectId!}' : '';
+
+    _messageProvider.send(
       message: '''
 $projectName ${const Uuid().v4()}
 <code>{
