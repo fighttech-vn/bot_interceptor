ac305ad74a1028f3aa7a34f908d5358c8d47eabd
HieuTranTrung <hieu.trantrung1204@gmail.com>
2022-10-07 17:50:52 +0700

    Update send telegram message long

--- lib/src/send_message/impl/telegram_send_message_provider.dart
+++ lib/src/send_message/impl/telegram_send_message_provider.dart
@@ -7,19 +7,32 @@ class TelegramSendMessageProvider extends SendMessageProvider {
   final int chatId;
   final String token;
 
-  TelegramSendMessageProvider({required this.chatId, required this.token})
-      : _bot = Bot(token: token);
+  TelegramSendMessageProvider({
+    required this.chatId,
+    required this.token,
+  }) : _bot = Bot(token: token);
 
   final Bot _bot;
 
   @override
   Future<void> send({required String message}) async {
-    try {
+    if (message.length < 4000) {
       await _bot.sendMessage(
         ChatID(chatId),
         message,
         parseMode: ParseMode.html,
       );
-    } catch (_) {}
+    } else {
+      await _bot.sendMessage(
+        ChatID(chatId),
+        message.substring(0, 3999),
+        parseMode: ParseMode.html,
+      );
+      await _bot.sendMessage(
+        ChatID(chatId),
+        message.substring(4000, message.length - 1),
+        parseMode: ParseMode.html,
+      );
+    }
   }
 }
